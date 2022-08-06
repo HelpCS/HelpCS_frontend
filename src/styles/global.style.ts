4e8170b9b3ba39f3f043e5809b95d59f20329e5f
hanseulhee <3021062@gmail.com>
2022-08-06 17:31:03 +0900

    [#1] Feat: 전역 스타일 임시 적용

---
+++ src/styles/global.style.ts
@@ -0,0 +1,16 @@
+import { globalCss } from "@stitches/react";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+    fontFamily: "Noto Sans KR, sans-serif",
+    wordBreak: "keep-all",
+  },
+  "html, body": {
+    width: "100vw",
+    height: "100vh",
+    scrollBehavior: "smooth",
+  },
+});
