dc2135cd2e65222b26c453a2719fe027a3f5f0e4
hanseulhee <3021062@gmail.com>
2022-08-06 22:11:49 +0900

    [#2] Feat: theme color 적용

--- stitches.config.ts
+++ stitches.config.ts
@@ -5,6 +5,9 @@ export const { styled, getCssText } = createStitches({
     colors: {
       white: "#FFFFFF",
       black: "#000000",
+      yellow: "#FDC453",
+      green: "0BB3A1",
+      grey100: "#E8E8EF",
     },
     fontWeights: {
       xs: 300,
