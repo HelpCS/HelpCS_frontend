a3e342f78912bfa611a48e14be645939a00db9be
hanseulhee <3021062@gmail.com>
2022-08-08 20:02:31 +0900

    [#7] logo 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { getCssText } from "../../stitches.config";
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -14,7 +14,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
-          {/* <link rel="icon" type="image/png" sizes="32x32" href="favicon/logo.png" /> */}
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon/logo.png" />
           <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
           <meta property="og:title" content="도와줘개발" />
           <meta property="og:description" content="오늘의 문제를 풀어보세요!" />
