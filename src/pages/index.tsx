2878ebc07af16b2cb6adc62054dc818b059cc2a6
hanseulhee <3021062@gmail.com>
2022-08-12 15:32:49 +0900

    [#13] Feat: GoLink component 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,30 @@
 import { styled } from "../../stitches.config";
-import AnswerLink from "../components/Button/AnswerLink";
+import GoLink from "../components/Button/GoLink";
 import QuestionCard from "../components/Card/QuestionCard";
 import Greetings from "../components/Text/Greetings";
 
 function Home() {
   return (
-    <>
+    <StyledPadding>
       <Greetings />
       <StyledItemWrapper>
         <QuestionCard />
-        <AnswerLink />
+        <GoLink
+          firstUrl="/"
+          twoUrl="/"
+          firstSummary="잘모르겠어요"
+          twoSummary="다른 문제 풀고 싶어요"
+        />
       </StyledItemWrapper>
-    </>
+    </StyledPadding>
   );
 }
 
 export default Home;
+
+const StyledPadding = styled("div", {
+  padding: "2rem 4rem",
+})
 
 const StyledItemWrapper = styled("div", {
   width: "100%",
