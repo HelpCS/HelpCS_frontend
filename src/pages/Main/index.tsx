4d2cf5026f2ef0596cab70d1c5c1349b6024636b
hanseulhee <3021062@gmail.com>
2022-08-16 22:33:25 +0900

    [#22] Feat: Main page button link

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,32 @@
+import { useRouter } from "next/router";
 import { styled } from "../../../stitches.config";
 import GoLink from "../../components/Button/GoLink";
 import QuestionCard from "../../components/Card/QuestionCard";
 import Greetings from "../../components/Text/Greetings";
 
 function Main() {
+  const router = useRouter();
+
+  function onClickWrong() {
+    router.push("/Wrong");
+  }
+
+  function onClickAnother() {
+    alert(
+      "'도와줘 개발' 서비스는 하루마다 한 문제가 제공됩니다. 내일 새로운 문제에 도전해보세요!"
+    );
+  }
+
   return (
     <StyledPadding>
       <Greetings />
       <StyledItemWrapper>
         <QuestionCard />
         <GoLink
-          firstUrl="/Wrong"
-          twoUrl="/"
+          onClickFirst={onClickWrong}
+          onClickTwo={onClickAnother}
           firstSummary="잘 모르겠어요"
-          twoSummary="다른 문제 풀고 싶어요"
+          secondSummary="다른 문제 풀고 싶어요"
         />
       </StyledItemWrapper>
     </StyledPadding>
@@ -24,7 +37,7 @@ export default Main;
 
 const StyledPadding = styled("div", {
   padding: "2rem 4rem",
-})
+});
 
 const StyledItemWrapper = styled("div", {
   width: "100%",
